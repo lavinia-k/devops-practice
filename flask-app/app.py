eb3eb139d024555e1be651f1f38cf2d7a005e992
Lavinia Kanagandran <lavinia.kanagandran@rokt.com>
2020-04-14 22:38:11 +1000

    Add app files and fix KmsKey ARN

---
+++ flask-app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+
+app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+@app.route("/")
+def home():
+    html = f"<h1>App Home</h1>"
+    return html.format(format)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
